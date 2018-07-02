fa40e10c9f6acd465b1cbcfa2d8e28525f127037
dcrowder2 <dcrowder2@alaska.edu>
2018-07-02 11:01:49 -0800

    Added functionallity for running on certain pixels and returning the descriptors

--- feature.py
+++ feature.py
@@ -19,13 +19,13 @@ def radial_points():
 # Get patch returns a cropped portion of the image provided using the globally defined radius
 # pixel is a tuple of (row, column) which is the row number and column number of the pixel in the picture
 # image is a cv2 image
-def get_patch(pixel, image):
+def get_patch(pixel, image, height, width):
 	radius = 6  # Used for patch size
 	diameter = 2 * radius
 	# max_row, max_col, not_used = np.array(image).shape Having this was making it super slow, so just manually put
 	# in the size of the images i guess
-	max_row = 2592
-	max_col = 3888
+	max_row = height
+	max_col = width
 	if pixel[0] >= (max_row - radius):
 		corner_row = max_row - (diameter + 2)
 		center_row = radius + pixel[0] - (max_row - (radius + 1))
@@ -35,7 +35,7 @@ def get_patch(pixel, image):
 	else:  # With the row coordinate being less than the radius of the patch, it has to be at the top of the image
 		corner_row = 0  # meaning the row coordinate for the patch will have to be 0
 		center_row = pixel[0]  # Because the pixel in question is less than the radius, the center of the patch
-								# should be the same as the pixel coming in as it should be less than 11
+	# should be the same as the pixel coming in as it should be less than 11
 
 	if pixel[1] >= (max_col - radius):
 		corner_col = max_col - (diameter + 2)
@@ -47,11 +47,31 @@ def get_patch(pixel, image):
 		corner_col = 0  # Image, meaning the column coordinate for the patch will have to be 0
 		center_col = pixel[1]  # same as the row
 	diameter += 1  # Added 1 for the center pixel
-	return image[corner_row:(corner_row+diameter), corner_col:(corner_col+diameter)], (center_row, center_col)
+	return image[corner_row:(corner_row + diameter), corner_col:(corner_col + diameter)], (center_row, center_col)
+
+
+def k_means_color(patch):
+	z = patch.reshape((-1, 3))
+	# Set the rgb values to be floats so it can be used in the k-means function
+	z = np.float32(z)
+	# Create the criteria for k-means clustering, 1st: Stop kmeans when the specified accuracy is met, or when the
+	# max iterations specified is met. 2nd: max iterations. 3rd: epsilon, or required accuracy
+	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+	k = 2
+	# run the K means clustering using cv2, so it can be done easily with images
+	# label and center being the important returns, with label being important for producing an image to show the clusters
+	# and center being useful for the NN and the producing the image to show the clusters, as its the average color of each
+	# cluster. Arguments for the kmeans, 1st: input data, 2nd: number of clusters needed, 3rd: not sure,
+	# 4th: the criteria specified above, 5th: number of times to run the clustering taking the best result, 6th: flags
+	ret, label, center = cv2.kmeans(z, k, None, criteria, 5, cv2.KMEANS_RANDOM_CENTERS)
+	center = np.uint8(center)  # Center will contain the Dominant Colors in their respective color channels
+	# i.e [[DCb, DCg, DCr], [DCb, DCg, DC3r]] with k = 2
+	return center
 
 
 # Returns the dominate colors in a patch, which are the average colors based upon what is the center of clusters that
 # are built from the rgb values in the patch, patch is a 3d array which is 50x50x3
+# TODO: use eigenvectors, don't use until it is using that method
 def get_dominate_color(patch):
 	b_root = patch[:, :, 0]
 	g_root = patch[:, :, 1]
@@ -70,8 +90,8 @@ def get_dominate_color(patch):
 	r_child_0 = r_root[r_root > r_root_mean]
 	r_child_1 = r_root[r_root <= r_root_mean]
 
-	center = [[[np.mean(b_child_0)], [np.mean(g_child_0)], [np.mean(r_child_0)]], [[np.mean(b_child_1)],
-				[np.mean(g_child_1)], [np.mean(r_child_1)]]]
+	center = [np.mean(b_child_0), np.mean(g_child_0), np.mean(r_child_0), np.mean(b_child_1), np.mean(g_child_1),
+	          np.mean(r_child_1)]
 	return center
 
 
@@ -84,11 +104,11 @@ def get_dominate_color(patch):
 def get_texture(patch, pixel, radial):
 	c_b, c_g, c_r = patch[pixel[0], pixel[1]]  # The rgb values of the center pixel
 	neighbor = [[], [], []]
-	if pixel[0] < 6 or pixel[1] < 6 or pixel[0] > 6 or pixel[1] >6:  # With the center not being the actual center,
+	if pixel[0] < 6 or pixel[1] < 6 or pixel[0] > 6 or pixel[1] > 6:  # With the center not being the actual center,
 		# neighbors are chosen at random
 		x = np.random.choice(12, 8)  # Very Important!! This needs to go to the size of the patch,
 		y = np.random.choice(12, 8)  # which is done manually, so if the size of the patch is changed this
-		#  needs to be as well
+	#  needs to be as well
 	else:
 		x = np.round(pixel[0] + radial[0])  # pixel is the x & y coordinate for the central pixel, which is the offset
 		y = np.round(pixel[1] + radial[1])  # for the neighbors, which is then rounded off so it is a whole number
@@ -103,31 +123,49 @@ def get_texture(patch, pixel, radial):
 	diff = [neighbor[0] - c_r, neighbor[1] - c_g, neighbor[2] - c_b]
 	pos_diff = np.array([diff[0][diff[0] > 0], diff[1][diff[1] > 0], diff[2][diff[2] > 0]])
 	neg_diff = np.array([diff[0][diff[0] < 0], diff[1][diff[1] < 0], diff[2][diff[2] < 0]])
-	return [np.sum(pos_diff[0]**2), np.sum(pos_diff[1]**2), np.sum(pos_diff[2]**2), np.sum(neg_diff[0]**2),
-			np.sum(neg_diff[1]**2), np.sum(neg_diff[2]**2)]
+	return [np.sum(pos_diff[0] ** 2), np.sum(pos_diff[1] ** 2), np.sum(pos_diff[2] ** 2), np.sum(neg_diff[0] ** 2),
+	        np.sum(neg_diff[1] ** 2), np.sum(neg_diff[2] ** 2)]
 
 
-def init(filepath):
-	for image in os.walk(filepath):
-		image_paths = image[2]
-	os.chdir(filepath)
-	images = np.array([cv2.imread(i) for i in image_paths])
-	return images
-
-
-if __name__ == '__main__':
-	path = "c:\\users\\dakot\\Desktop\\metal scraps\\"
-	start_time = time.time()
+def run_pixels(image, coordinates):
+	h, w = image.shape[:2]
 	radial = radial_points()
-	images = init(path)
-	t_time = time.time()
-	print("Images loaded:" + str(t_time - start_time))
-	total_time = t_time
-	temp = images[0]
-	for i in range(0, 2592):  # TODO: add parallelism, saving of the features
-		for j in range(0, 3888):
-			center_pixel = (i, j)
-			patch, pixel = get_patch(center_pixel, temp)
-			descriptor_color = get_dominate_color(patch)
-			descriptor_texture = get_texture(patch, pixel, radial)
-	print("Final time: " + str(time.time() - total_time))
+	return_array = []
+	for coordinate in coordinates:
+		array = []
+		patch, pixel = get_patch(coordinate, image, h, w)
+		descriptor_color = k_means_color(patch)
+		descriptor_texture = get_texture(patch, pixel, radial)
+		array.extend(coordinate)
+		array.extend(descriptor_texture)
+		array.extend(descriptor_color[0])
+		array.extend(descriptor_color[1])
+		return_array.append(array)
+	return return_array
+
+
+# The rest of the code is for running an entire folder of images
+# def init(filepath):
+# 	for image in os.walk(filepath):
+# 		image_paths = filepath + "\\" + image[2]
+# 	images = np.array([cv2.imread(i) for i in image_paths])
+# 	return images
+#
+#
+# if __name__ == '__main__':
+# 	path = "c:\\users\\dakot\\Desktop\\metal scraps\\"
+# 	start_time = time.time()
+# 	radial = radial_points()
+# 	images = init(path)
+# 	t_time = time.time()
+# 	print("Images loaded:" + str(t_time - start_time))
+# 	total_time = t_time
+# 	for image in images:
+# 		h, w = image.shape[:2]
+# 		for i in range(0, h):
+# 			for j in range(0, w):
+# 				center_pixel = (i, j)
+# 				patch, pixel = get_patch(center_pixel, image, h, w)
+# 				descriptor_color = get_dominate_color(patch)
+# 				descriptor_texture = get_texture(patch, pixel, radial)
+# 	print("Final time: " + str(time.time() - total_time))
